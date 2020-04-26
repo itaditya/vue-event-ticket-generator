54d92432ad8b5c05dc6327e3ed9896c678411b86
Aditya Agarwal <adityaa803@gmail.com>
2020-04-26 13:40:29 +0530

    added basic markup & styles

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,102 @@
 <template>
   <div id="app">
-    <p>Hello</p>
-    <HelloWorld />
+    <div class="tickets-control no-print">
+      <form class="tickets-form">
+        <label>
+          <span>Enter Event Name</span>
+          <input placeholder="Eg: Vue.js Workshop" />
+        </label>
+        <label>
+          <span>Enter Event Organiser</span>
+          <input placeholder="Eg: Mozilla Bbsr" />
+        </label>
+        <label>
+          <span>Enter Event Price</span>
+          <input placeholder="Eg: 20" />
+        </label>
+      </form>
+      <div>
+        <p>Fill the event details in the form. <br />If you like the generated tickets then click on Print button</p>
+        <button>Print Tickets</button>
+      </div>
+    </div>
+    <ul class="tickets-list">
+      <li>
+        <div class="ticket-wrapper">
+          <Ticket />
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ticket from './components/Ticket';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Ticket,
+  },
+};
 </script>
 
 <style>
+body,
+ul,
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.tickets-control {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 40px;
+}
+
+.tickets-form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.tickets-list {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.ticket-wrapper {
+  width: 350px;
+  padding: 5px;
+}
+
+@media print {
+  body {
+    -webkit-print-color-adjust: exact;
+  }
+
+  .no-print,
+  .no-print * {
+    display: none !important;
+  }
 }
 </style>
---
+++ src/components/Ticket.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="ticket">
+    <div class="ticket-column-1">
+      <div>
+        <h6 class="ticket-heading">Event Name</h6>
+        <p class="ticket-info">Event Organiser</p>
+      </div>
+      <div>
+        <span class="ticket-price">$20</span>
+        <span>&nbsp;/-</span>
+      </div>
+    </div>
+    <div class="ticket-column-2">
+      <div class="ticket-qr">
+        QR Code
+      </div>
+      <p class="ticket-code">ID: 8y37343834</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ticket",
+};
+</script>
+
+<style scoped>
+.ticket {
+  background-color: hsl(243.3, 12.5%, 28.2%);
+  color: #fff;
+  padding: 15px 20px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.ticket-column-1 {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-bottom: 5px;
+}
+
+.ticket-heading {
+  margin: 0;
+  font-size: 20px;
+  color: hsl(180, 32.3%, 62.9%);
+}
+
+.ticket-info {
+  margin: 0;
+  margin-top: 10px;
+}
+
+.ticket-price {
+  font-size: 30px;
+}
+
+.ticket-column-2 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.ticket-qr {
+  width: 100px;
+  height: 100px;
+}
+
+.ticket-code {
+  margin: 0;
+  margin-top: 5px;
+}
+</style>
